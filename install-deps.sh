888a8b9511b4130bfcc97bf80120856c93b41164
xrombik <xrombik@yandex.ru>
2019-10-22 00:00:56 +0300

    CI

--- install-deps.sh
+++ install-deps.sh
@@ -6,23 +6,18 @@ set -e
 # Only for *.deb operating systems (Debian, Ubuntu, Mint, etc.).
 PACKS=("python-gtk2" "libgtkglext1" "python-opengl" "python-gtkglext1" "python-pip")
 
-apt-get autoremove -f -y
-apt-get update -y
-apt-get upgrade -y
+sudo apt-get autoremove -f -y
+sudo apt-get update -y
+sudo apt-get upgrade -y
 
 for PACK in "${PACKS[@]}"
 do
 	echo -n "Check for package availability \"$PACK\" ... "
-	dpkg -s $PACK > /dev/null
-	if [ $? -ne 0 ]; then
+	dpkg -s ${PACK} > /dev/null
+	if [[ $? -ne 0 ]] ; then
 		echo "\"$PACK\" hasn't been found, and will be installed."
-		apt-get install $PACK -y
+		sudo apt-get install ${PACK} -y
 	else
 		echo " found."
 	fi
 done
-
-pip install --upgrade pip
-pip install setuptools
-pip install freetype-py
-pip install typing
