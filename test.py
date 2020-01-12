4f46bd83a7295813b5b61c867be0404347622439
xrombik <xrombik@yandex.ru>
2020-01-13 01:01:53 +0300

    Улучшено тестирование

--- test.py
+++ test.py
@@ -1,11 +1,37 @@
 import unittest
-
+import subprocess
+import os
+import signal
+import time
+import fcntl
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
+
+    def test_imports(self):
         import main
-        print('%s' % str(main))
+        print (u'%s\n%s' % (self, main))
+
+    def test_screenshot(self):
+        print (u'%s' % self)
+        pid = subprocess.Popen(
+            'python main.py &',
+            shell=True,
+            preexec_fn=os.setsid,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE)
+        fd = pid.stdout.fileno()
+        flags = fcntl.fcntl(fd, fcntl.F_GETFL)
+        fcntl.fcntl(fd, fcntl.F_SETFL, flags | os.O_NONBLOCK)
+        time.sleep(2)
+        try:
+            rc = pid.stdout.read()
+            if 'traceback' in rc:
+                self.fail()
+        except IOError:
+            print("OK")
+        os.system('./screenshot.sh')
+        os.killpg(pid.pid, signal.SIGTERM)
 
 
 if __name__ == '__main__':
-    unittest.main()
+    unittest.main(failfast=True)
